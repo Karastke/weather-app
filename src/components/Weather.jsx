c4fc68f4c715f5ff43f48e35b1a72e1d9bc72812
Karastke <fkdlsaos1@naver.com>
2024-05-06 17:38:47 +0900

    refactor: adjust gray box div container
    회색 상자의 크기를 절대 값 대신 백분율로 수정해 반응형 웹 디자인에 적합하게 만들었음.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,8 +5,8 @@ const Weather = ({ weatherData }) => {
   return (
     <div>
       {weatherData.weather && weatherData.weather.length > 0 ? (
-        <div className='w-[500px] h-[250px] bg-gray-300 shadow-lg rounded-xl
-          m-auto relative px-6 top-[10%]'>
+        <div className='w-[100%] h-[100%] max-width: 500px bg-gray-300 shadow-lg rounded-xl
+          m-auto relative px-5 top-[10%] py-3'>
           <div className="flex justify-between w-full">
             <div className='w-1/2 my-4 mx-auto flex justify-between items-center'>
               <div className="flex flex-col 
@@ -27,7 +27,7 @@ const Weather = ({ weatherData }) => {
             </div>
 
             <div className='w-1/2 flex flex-col justify-between items-end'>
-            <div className='relative'>
+                <div className='relative'>
                 <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="" className='w-[120px]' />
             </div>
                 {weatherData.name !== undefined ? (
@@ -43,6 +43,7 @@ const Weather = ({ weatherData }) => {
                             <p className='font-bold w-20'>
                                 {weatherData.main.humidity} %
                             </p>
+                            
                         </div>
                         <div className="flex-justify-evenly gap-x-8">
                             <p>Wind Speed</p>
