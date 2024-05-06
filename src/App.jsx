372fe66020f6844bda36911c8e51f0366b1bf492
Karastke <fkdlsaos1@naver.com>
2024-05-06 11:54:19 +0900

    feat: add text input
    위치 입력을 위한 text box 창을 추가함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import axios from 'axios'
+import './index.css'
+import { useState } from 'react'
 
 function App() {
 
-
   return (
-    <div>
-    
+   
+  <div className='w-full h-full relative'>
+      <div className='text-center p-4'>
+      <input type="text" className='py-3 px-6 w-[700px]
+      text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400
+      focus:outline-none bg-white-600/100 shadow-md' placeholder='위치를 입력하세요.' 
+      value={location}
+      onChange={(event) => setLocation(event.target.value)}
+      onKeyDownCapture={searchLocation}/>
+      </div>
     </div>
   )
 }
