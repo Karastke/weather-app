1e120b86523418afcfb1ad2de4ef10b2e544c43f
Karastke <fkdlsaos1@naver.com>
2024-05-06 12:14:09 +0900

    feat: add API and its function
    OpenWeatherMap의 API 및 위치 검색에 관련된 로직을 구현함.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ import { useState } from 'react'
 
 function App() {
 
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState("")
+
+  const API_KEY = "b641c1f69bb6c4da9b041d57648c9a39"
+  const url = `https://api.openweathermap.org/data/2.5./weather?q=${location}&appid=
+  ${API_KEY}`
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        }).catch((error) => {
+          console.error("API 에러:", error);
+        });
+        setLocation("");
+    }
+  }
   return (
-   
-  <div className='w-full h-full relative'>
+    <div className='w-full h-full relative'>
       <div className='text-center p-4'>
       <input type="text" className='py-3 px-6 w-[700px]
       text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400
